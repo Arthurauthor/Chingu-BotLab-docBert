4a173315385cb28de94c687133b1a0e01cab6e80
winfredselwyn <winfredselwyn@gmail.com>
2017-03-13 17:52:39 +0800

    feat: Added example collection to DB, and S3 packages.
    Added minio and aws-sdk. Try both to find which suits docBert best.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,17 @@ import os from 'os';
 import mongoose from 'mongoose';
 import { API_TOKEN } from './constants';
 
+// connect to local database
+mongoose.connect('mongodb://127.0.0.1:27017/docBertDB');
+
+// initialize a mongoDB collection
+const reference = mongoose.model('reference',
+                                  { 
+                                    filename: 'string', // example schema
+                                    url: 'https://s3.amazon.com/926ibCU212/', // example schema
+                                  }
+                                )
+
 const controller = Botkit.slackbot({
   debug: false,
 });
